f1e97481e2bbc19e5b001e3195c37299e03f550e
Justman10000 <justmegaliga10000@gmail.com>
2022-05-10 22:20:58 +0200

    German added & Spelling on EnglishUK

---
+++ modules/RconMC/language/German/general.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/tree/v2/
+ *  NamelessMC version 2.0.0-pr7
+ *
+ *  License: MIT
+ *
+ *  RconMC By xGIGABAITx
+ */
+
+$language = array(
+	'rcon_name' => 'RCON',
+	'new_server' => 'Server hinzufügen',
+	'server_edit_title' => 'Rcon Server bearbeiten:',
+	'group_permision' => 'Admin Berechtigungen',
+	'server_permision' => 'Zugang zum RCON Server: ',
+	'server_veiw' => 'Zugriff auf die Serverlistenseite',
+	'no_servers' => 'KEINE SERVER',
+	'new_server_successfully' => 'New server added successfully',
+	'edit_server_successfully' => 'Server edited successfully',
+	'deleted_successfully' => 'Erfolgreich gelsöcht',
+	'edit_errors' => 'Bearbeiten fehlgeschlagen',
+	'new_server_errors' => 'Neuen Server hinzufügen fehlgeschlagen',
+	'server_edit' => 'Server Bearbeitung',
+	'add_new_server' => 'Neuen Server hinzufügen',
+	'server_name' => 'Name',
+	'server_ip' => 'IP Adresse',
+	'server_port' => 'Port',
+	'server_pass' => 'Passwort',
+	'send_title' => 'Sende einen Befhl zu dem Server:',
+	'error_rcon_connect' => 'RCON connection error',
+	'error_commands' => 'Leerer Befehl',
+	'error_commands_label' => 'Fehler',
+	'success_send_command' => 'Befehl erfolgreich gesendet',
+	'console' => 'Konsole',
+	'no_response' => 'Keine Antwort',
+	'command_title' => 'Befehl:',
+	'response_title' => 'Antwort:',
+	'restart' => 'NEUSTART',
+	'stop' => 'STOP',
+	'tps' => 'TPS'
+);
